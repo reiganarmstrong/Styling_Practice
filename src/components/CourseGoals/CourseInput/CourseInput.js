670972579c0cc71d256654daa32fa1b1c4070532
Reagan Armstrong <rarmst@reagans-mbp.myfiosgateway.com>
2022-06-15 19:52:22 -0400

    css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,23 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import styles from "./CourseInput.module.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
-
-  const goalInputChangeHandler = event => {
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+    } else {
+      props.onAddGoal(enteredValue);
+    }
   };
-
+  // let labelColor = {};
+  // let inputTextStyle = {};
+  // if (!isValid) {
+  //   inputTextStyle = { borderColor: "red", background: "salmon" };
+  //   labelColor = { color: "red" };
+  // } else {
+  //   labelColor = { color: "black" };
+  //   inputTextStyle = {
+  //     borderColor: "#ccc",
+  //     background: "transparent",
+  //   };
+  // }
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
